17e4b794948c9793a0b94e477307fc27ccffb8dd
Michael Campbell <mtcampbe@illinois.edu>
2020-04-25 22:05:23 -0500

    Update to use new juketest utils

--- utils/PyJuKe/CheckTest.py
+++ utils/PyJuKe/CheckTest.py
@@ -1,5 +1,5 @@
-import re
 import sys
+import juketest
 
 numArgs = len(sys.argv)
 if(numArgs != 3):
@@ -11,15 +11,16 @@ resultsFileName = sys.argv[2]
 print("CheckTest: Checking ", resultsFileName,
       " for Test(", testName, ")")
 
-resultsFile = open(resultsFileName, 'r')
+testResults = juketest.ReadTestResults(resultsFileName)
+checkTest = juketest.CheckResult(testResults, testName)
 
-for line in resultsFile:
-    if(re.search(testName, line)):
-        if(re.search("PASS", line)):
-            print("CheckTest: Test(", testName, ") PASSED.")
-            sys.exit(0)
-        else:
-            print("CheckTest: Test(", testName, ") FAILED.")
-            sys.exit(1)
-print("CheckTest: Test(", testName, ") MISSING.")
+if(checkTest == 0):
+    print("CheckTest: Test("+testName+") PASSED.")
+    sys.exit(0)
+else:
+    if(checkTest > 0):
+        print("CheckTest: Test("+testName+") FAILED.")
+    else:
+        print("CheckTest: Test("+testName+") MISSING.")
+    sys.exit(1)
 sys.exit(1)
