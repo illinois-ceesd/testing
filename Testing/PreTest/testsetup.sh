498f3c2de5ff843b4ffc0a836bc1892d2c19034a
Michael Campbell <mtcampbe@illinois.edu>
2020-05-05 11:54:51 -0500

    Added Post/Pre-test suite to allow user to run stuff before/after testing is done. Automatically starts/wraps up with testsetup.sh/donetesting.sh

---
+++ Testing/PreTest/testsetup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sourcedir=${1}
+bindir=${2}
+
+printf "ReadyToTest: SourcePath = ${sourcedir}\n"
+printf "ReadyToTest: BinPath    = ${bindir}\n"
+printf "ReadyToTest: Tests are ready!\n"
+
