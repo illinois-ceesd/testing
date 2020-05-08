a85f6bf56c94d18c044cb2fa61b501aff7343a29
Michael Campbell <mtcampbe@illinois.edu>
2020-05-08 08:09:38 -0500

    tweaked gnuplot plotting commands for bw plot

--- Testing/PostTest/MakeZAXPYGridscalePlots.py
+++ Testing/PostTest/MakeZAXPYGridscalePlots.py
@@ -16,6 +16,7 @@ imagepath = cwd + "/zaxpy_gridscale_" + localrev + ".png"
 datapath = cwd + "/zaxpy_gridscale_timing"
 testname = "zaxpy_gridscale_plot"
 tmpdatapath = cwd + "/zaxpy_gridscale_data.txt"
+machinename = juke.buildhost
 
 if path.exists(datapath) is False:
     print(testname + ": Datapath(" + datapath + ") did not exist.")
@@ -28,21 +29,29 @@ if path.exists(tmpdatapath):
 
 os.system("touch "+tmpdatapath)
 gridsize = 16
-while gridsize < 2048:
+while gridsize < 1024:
+    numbytes = pow(gridsize,3) * 24.0 / (pow(1024,2))
+    os.system('printf "' + str(numbytes) + ' " >> ' + tmpdatapath)
     os.system('grep "zaxpy3\-' + str(gridsize) +
               '" ' + datapath + ' >> ' + tmpdatapath)
     gridsize = 2*gridsize
 
 
 gp.default_term = ''
+gp.c('set ylabel font "Helvetica, 20"')
+gp.c('set xlabel font "Helvetica, 20"')
+gp.c('set key font "Helvetica, 16"')
+gp.c('set tic font "Arial, 15"')
+gp.c('set logscale x 2')
+gp.c('set logscale y 2')
 gp.c('set term png')
 gp.c('set boxwidth 0.5')
 gp.c('set style fill solid')
-gp.c('set title "Loopy/ZAXPY grid scaling"')
+gp.c('set title "Loopy/ZAXPY on ' + machinename + '"')
 gp.c('set output "' + imagepath + '"')
-gp.c('set ylabel "time(s)"')
-gp.c('set xlabel "gridsize"')
-gp.c('plot "' + tmpdatapath + '" u 0:3:xtic(1) w boxes t ""')
+gp.c('set ylabel "Bandwidth (GB/s)"')
+gp.c('set xlabel "DataSize (MB)"')
+gp.c('plot "' + tmpdatapath + '" u 1:(($1/1024.0)/$4) w lp t "Loopy/ZAXPY BW"')
 
 time.sleep(1.0)
 
