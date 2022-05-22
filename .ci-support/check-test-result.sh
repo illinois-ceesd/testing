5c1703835c65759ddc256a48f02855dcd43c4fa9
Michael Campbell <mtcampbe@illinois.edu>
2022-05-22 09:22:20 -0500

    Sharpen the scripts and comments a bit

--- .ci-support/check-test-result.sh
+++ .ci-support/check-test-result.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+# Usage: check-test-result.sh <data_path> <suite_name> <test_name>
+# 
+# This script checks for a testing result by looking in the
+# <data_path> directory for the following files:
+# (1) <data_path>/testing-latest-timestamp.txt => <timestamp>
+# (2) <data_path>/testing-log-<suite_name>-<testing_host>-<timestamp>.txt
+# (3) <data_path>/testing-results-<suite_name>-<testing_host>-<timestamp>.txt
+#
+# CI is the main user of this script.  The call to it can be configured in
+# > testing/.github/workflows/ci.yaml
+# CI examines the return code of this script to indicate whether the test
+# passes (0) or fails (non-zero).
+#
 testing_host=${1}
 suite_name=${2}
 test_name=${3}
-
 data_path=${testing_host}
 
 origin=$(pwd)
@@ -14,6 +26,7 @@ then
     exit 1
 fi
 
+# The timestamp for the latest tests is recorded in this file.
 timestamp_filename="${data_path}/testing-latest-timestamp.txt"
 
 if [ ! -f "${timestamp_filename}" ];
@@ -32,6 +45,8 @@ fi
 
 printf "Found testing timestamp: ${timestamp}\n"
 
+# The testing (pass=0, fail!=0) results for the whole suite
+# are in this file. In the form "test-name: 0". 
 testing_resultsfile="${data_path}/testing-results-${suite_name}-${testing_host}-${timestamp}.txt"
 
 if [ ! -f "${testing_resultsfile}" ];
@@ -40,9 +55,12 @@ then
     exit 1
 fi
 
+# The extra prints improve transparency at the CI level
 printf "Found latest test results file: (${testing_resultsfile}).\n"
 printf "Grabbing results for test (${test_name}).\n"
-
+printf "The expected result is, in general: 'test-name: 0'\n"
+printf "In this case, ('test-name') == (${test_name}).\n"
+printf "Anything other than the expected result is a fail.\n"
 test_result_line=$(grep "^${test_name}:" ${testing_resultsfile})
 
 if [ "${test_result_line}" == "" ];
@@ -51,7 +69,7 @@ then
     exit 1
 fi
 
-printf "Testing results line: ($test_result_line).\n"
+printf "Found testing results line: ($test_result_line).\n"
 
 if [ "${test_result_line}" == "${test_name}: 0" ];
 then
--- .ci-support/get-package-testing-log.sh
+++ .ci-support/get-package-testing-log.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Usage: get-package-testing-log.sh <data_path> <suite_name>
+
+# This script checks for and spews a testing output
+# (i.e., any stdout/err created by the package as it is tested),
+# and looks in the <data_path> directory for the following files:
+# (1) <data_path>/testing-latest-timestamp.txt => <timestamp>
+# (2) <data_path>/testing-log-<suite_name>-<testing_host>-<timestamp>.txt
+#
+# CI is the main user of this script.  The call to it can be configured in
+# > testing/.github/workflows/ci.yaml
+# CI examines the return code of this script to indicate whether the test
+# passes (0) or fails (non-zero).
+#
 data_path=${1}
 suite_name=${2}
 testing_host=${data_path}
@@ -10,6 +23,7 @@ then
     exit 1
 fi
 
+# The timestamp for the latest tests is recorded in this file.
 timestamp_filename="${data_path}/testing-latest-timestamp.txt"
 
 if [ ! -f "${timestamp_filename}" ];
@@ -30,6 +44,10 @@ printf "Found testing timestamp: ${timestamp}\n"
 
 testing_logfile="${data_path}/testing-log-${suite_name}-${testing_host}-${timestamp}.txt"
 
+printf "The expected filename is: (<data_path>/testing-log-<suite_name>-<testing_host>-<timestamp>.txt)\n"
+printf "In this case: (${testing_logfile})\n"
+printf "If the file does not exist, then this phase of the test fails.\n"
+
 if [ ! -f "${testing_logfile}" ];
 then
     printf "Testing output log ($testing_logfile) not found.\n"
@@ -37,4 +55,5 @@ then
 fi
 
 printf "Found testing output log: ($testing_logfile).\n"
+# Just spew the contents of the file to the CI output
 cat $testing_logfile
