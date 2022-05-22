f63ae6f843374671bc575a2fc5b78d3c7b56fee7
Mike Campbell <mtcampbe@illinois.edu>
2022-05-22 09:36:45 -0500

    Add isolator testing to lassen nightly , generalize infrastructure (#6)

--- .ci-support/check-test-result.sh
+++ .ci-support/check-test-result.sh
@@ -1,18 +1,81 @@
 #!/bin/bash
 
-platform_path=${1}
-test_pattern=${2}
+# Usage: check-test-result.sh <data_path> <suite_name> <test_name>
+# 
+# This script checks for a testing result by looking in the
+# <data_path> directory for the following files:
+# (1) <data_path>/testing-latest-timestamp.txt => <timestamp>
+# (2) <data_path>/testing-log-<suite_name>-<testing_host>-<timestamp>.txt
+# (3) <data_path>/testing-results-<suite_name>-<testing_host>-<timestamp>.txt
+#
+# CI is the main user of this script.  The call to it can be configured in
+# > testing/.github/workflows/ci.yaml
+# CI examines the return code of this script to indicate whether the test
+# passes (0) or fails (non-zero).
+#
+testing_host=${1}
+suite_name=${2}
+test_name=${3}
+data_path=${testing_host}
 
-latest_logfile=$(ls -t ${platform_path}/testing-log* | head -1)
-latest_resultsfile=$(ls -t ${platform_path}/testing-results* | head -1)
+origin=$(pwd)
 
-if [ "${latest_logfile}" == "" ];
+if [ ! -d ${data_path} ];
 then
-    printf "Test did not run.\n"
+    printf "Data path (${data_path}) does not exist.\n"
     exit 1
 fi
 
-cat $latest_logfile
-return_code=$(grep ${test_pattern} ${latest_resultsfile} | cut -d ":" -f 2)
+# The timestamp for the latest tests is recorded in this file.
+timestamp_filename="${data_path}/testing-latest-timestamp.txt"
 
-exit $(($return_code+0))
+if [ ! -f "${timestamp_filename}" ];
+then
+    printf "Testing timestamp file (${timestamp_filename}) not found.\n"
+    exit 1
+fi
+
+timestamp=$(cat ${timestamp_filename})
+
+if [ "${timestamp}" == "" ];
+then
+    echo "Testing timestamp is missing."
+    exit 1
+fi
+
+printf "Found testing timestamp: ${timestamp}\n"
+
+# The testing (pass=0, fail!=0) results for the whole suite
+# are in this file. In the form "test-name: 0". 
+testing_resultsfile="${data_path}/testing-results-${suite_name}-${testing_host}-${timestamp}.txt"
+
+if [ ! -f "${testing_resultsfile}" ];
+then
+    printf "Testing results file (${testing_resultsfile}) not found.\n"
+    exit 1
+fi
+
+# The extra prints improve transparency at the CI level
+printf "Found latest test results file: (${testing_resultsfile}).\n"
+printf "Grabbing results for test (${test_name}).\n"
+printf "The expected result is, in general: 'test-name: 0'\n"
+printf "In this case, ('test-name') == (${test_name}).\n"
+printf "Anything other than the expected result is a fail.\n"
+test_result_line=$(grep "^${test_name}:" ${testing_resultsfile})
+
+if [ "${test_result_line}" == "" ];
+then
+    printf "Result for test (${test_name}) not found.\n"
+    exit 1
+fi
+
+printf "Found testing results line: ($test_result_line).\n"
+
+if [ "${test_result_line}" == "${test_name}: 0" ];
+then
+    echo "Test passed."
+    exit 0
+fi
+
+echo "Test failed."
+exit 1
--- mirge-test.sh
+++ mirge-test.sh
@@ -4,10 +4,13 @@ set -e
 set -x
 set -o pipefail
 
+testing_configfile=${1}
 date
 timestamp=$(date "+%Y.%m.%d-%H.%M.%S")
 TIME_SINCE_EPOCH=$(date +%s)
 TESTING_RUN_HOME=$(pwd)
+testing_configfile="${TESTING_RUN_HOME}/${testing_configfile}"
+
 TESTING_RUN_HOSTNAME=$(hostname)
 if [[ "$TESTING_RUN_HOSTNAME" == "lassen"* ]]; then
     TESTING_RUN_HOST="lassen"
@@ -22,20 +25,11 @@ TESTING_RUN_ARCH=$(uname -m)
 # own pkg-specific or fork-specific repo.
 # CEESD nightly testing writes here:
 TESTING_RESULTS_REPO="illinois-ceesd/testing.git"
-TESTING_RESULTS_BRANCH="main"
-TESTING_LOGFILE_NAME="${TESTING_RUN_HOME}/testing-log-${TESTING_RUN_HOST}-${timestamp}.txt"
-TESTING_RESULTSFILE_NAME="${TESTING_RUN_HOME}/testing-results-${TESTING_RUN_HOST}-${timestamp}.txt"
+TESTING_RESULTS_BRANCH="add-isolator-test"
 TESTING_ENV_NAME="ceesd.testing.env"
 MIRGE_PKG_FORK="illinois-ceesd"
 MIRGE_PKG_REPO="${MIRGE_PKG_FORK}/mirgecom"
 MIRGE_PKG_BRANCH="production"
-
-# For each "driver package" to test
-#TESTING_PKG_NAME="y2-isolator"
-TESTING_PKG_NAME="mirgecom"
-TESTING_PKG_FORK="illinois-ceesd"
-TESTING_PKG_REPO="${TESTING_PKG_FORK}/${TESTING_PKG_NAME}"
-TESTING_PKG_BRANCH="production"
 
 # -- Install conda env, dependencies and MIRGE-Com via *emirge*
 # --- remove old run if it exists
@@ -43,7 +37,7 @@ if [ -f "INSTALL_MIRGECOM" ] || [ ! -d "emirge" ];
 then
     if [ -d "emirge" ];
     then
-        echo "Removing old timing run."
+        echo "Removing old installation."
         mv -f emirge emirge.old
         rm -rf emirge.old &
     fi
@@ -63,59 +57,87 @@ source ${EMIRGE_HOME}/config/activate_env.sh
 cd emirge/mirgecom
 git pull
 
-# --- Grab the case driver repo
-if [ "${TESTING_PKG_NAME}" != "mirgecom" ]
-then
-    if [ -f "INSTALL_${TESTING_PKG_NAME}" ]
+# For each "driver package" to test
+while read -r line
+do
+    cd ${TESTING_RUN_HOME}/emirge/mirgecom
+
+    printf "Testing config: ${line}\n"
+    TESTING_PKG_NAME=$(printf "${line}" | cut -d "|" -f 1)
+    TESTING_PKG_REPO=$(printf "${line}" | cut -d "|" -f 2)
+    TESTING_PKG_BRANCH=$(printf "${line}" | cut -d "|" -f 3)
+
+    TESTING_LOGFILE_NAME="${TESTING_RUN_HOME}/testing-log-${TESTING_PKG_NAME}-${TESTING_RUN_HOST}-${timestamp}.txt"
+    TESTING_RESULTSFILE_NAME="${TESTING_RUN_HOME}/testing-results-${TESTING_PKG_NAME}-${TESTING_RUN_HOST}-${timestamp}.txt"
+
+    rm -f ${TESTING_LOGFILE_NAME} ${TESTING_RESULTSFILE_NAME}
+
+    printf "# ----- Automated testing logfile ($TESTING_RUN_DATE) ----\n" > ${TESTING_LOGFILE_NAME} 
+    printf "# ----- Automated testing resultsfile ($TESTING_RUN_DATE) ----\n" > ${TESTING_RESULTSFILE_NAME}
+
+    # --- Grab the case driver repo
+    if [ "${TESTING_PKG_NAME}" != "mirgecom" ]
     then
-        rm -Rf ${TESTING_PKG_NAME}
-        git clone -b ${TESTING_PKG_BRANCH} git@github.com:/${TESTING_PKG_REPO} ${TESTING_PKG_NAME}
+        if [ -f "${TESTING_RUN_HOME}/INSTALL_${TESTING_PKG_NAME}" ] || [ ! -d "${TESTING_PKG_NAME}" ];
+        then
+            rm -Rf ${TESTING_PKG_NAME}
+            git clone -b ${TESTING_PKG_BRANCH} git@github.com:/${TESTING_PKG_REPO} ${TESTING_PKG_NAME}
+            rm "${TESTING_RUN_HOME}/INSTALL_${TESTING_PKG_NAME}"
+        fi
+        cd ${TESTING_PKG_NAME}
     fi
-    cd ${TESTING_PKG_NAME}
-fi
 
-TESTING_PKG_HASH=$(git rev-parse HEAD)
-
-# automatically look for the 'testing' directory
-if [ ! -d "testing" ]
-then
-    printf "Testing error: did not find testing directory for testing runs. Aborting tests for ${TESTING_PKG_NAME}.\n"
-    exit 1
-fi
-
-cd testing
-printf "Running ${TESTING_PKG_NAME} tests on Host: ${TESTING_RUN_HOST}\n"
-date
-
-TESTING_SCRIPT_NAME="test-${TESTING_RUN_HOST}.sh"
-if [ ! -f "${TESTING_SCRIPT_NAME}" ]
-then
-    printf "Testing script (${TESTING_SCRIPT_NAME}) not found.\n"
-    TESTING_SCRIPT_NAME="test-${TESTING_RUN_PLATFORM}.sh"
+    TESTING_PKG_HASH=$(git rev-parse HEAD)
+    
+    # automatically look for the 'testing' directory
+    if [ ! -d "testing" ]
+    then
+        printf "Testing error: did not find testing directory for testing runs. Aborting tests for ${TESTING_PKG_NAME}.\n"
+        printf "${TESTING_PKG_NAME}-package-testing: 1\n" >> ${TESTING_RESULTSFILE_NAME}
+        continue        
+    fi
+    
+    cd testing
+    printf "Running ${TESTING_PKG_NAME} tests on Host: ${TESTING_RUN_HOST}\n"
+    date
+    
+    TESTING_SCRIPT_NAME="test-${TESTING_RUN_HOST}.sh"
     if [ ! -f "${TESTING_SCRIPT_NAME}" ]
     then
-        printf "Testing script (${TESTING_SCRIPT_NAME}) not found, aborting test.\n"
-        exit 1
+        printf "Testing script (${TESTING_SCRIPT_NAME}) not found.\n"
+        TESTING_SCRIPT_NAME="test-${TESTING_RUN_PLATFORM}.sh"
+        if [ ! -f "${TESTING_SCRIPT_NAME}" ]
+        then
+            printf "Testing script (${TESTING_SCRIPT_NAME}) not found, aborting test.\n"
+            printf "${TESTING_PKG_NAME}-package-testing: 1\n" >> ${TESTING_RESULTSFILE_NAME}
+            continue
+        fi
     fi
-fi
 
-printf "Testing with testing script (./${TESTING_SCRIPT_NAME}).\n"
-printf "# Automated testing logfile for testing script (${TESTING_SCRIPT_NAME})\n" > ${TESTING_LOGFILE_NAME} 
-printf "# Automated testing resultsfile for testing script (${TESTING_SCRIPT_NAME})\n" > ${TESTING_RESULTSFILE_NAME}
-./${TESTING_SCRIPT_NAME} ${EMIRGE_HOME} ${TESTING_RESULTSFILE_NAME} ${TESTING_LOGFILE_NAME}
-TESTING_SCRIPT_STATUS=$?
+    printf "Testing with testing script (./${TESTING_SCRIPT_NAME}).\n"
+    printf "# Automated testing logging for testing script (${TESTING_SCRIPT_NAME})\n" >> ${TESTING_LOGFILE_NAME} 
+    printf "# Automated testing resultsfile capture for testing script (${TESTING_SCRIPT_NAME})\n" >> ${TESTING_RESULTSFILE_NAME}
+    ./${TESTING_SCRIPT_NAME} ${EMIRGE_HOME} ${TESTING_RESULTSFILE_NAME} ${TESTING_LOGFILE_NAME}
+    TESTING_SCRIPT_STATUS=$?
+    printf "# Return status for testing script (${TESTING_SCRIPT_NAME}): ${TESTING_SCRIPT_STATUS}\n" >> ${TESTING_LOGFILE_NAME}
+    printf "${TESTING_PKG_NAME}-package-testing: ${TESTING_SCRIPT_STATUS}\n" >> ${TESTING_RESULTSFILE_NAME}
+    cd ../../
+
+done < ${testing_configfile}
 
 printf "Testing done for all packages.\n"
 date
 
+rm -rf testing-run-results
 git clone -b ${TESTING_RESULTS_BRANCH} git@github.com:/${TESTING_RESULTS_REPO} testing-run-results
 mkdir -p testing-run-results/${TESTING_RUN_HOST}
-cp ${TESTING_LOGFILE_NAME} testing-run-results/${TESTING_RUN_HOST}
-cp ${TESTING_RESULTSFILE_NAME} testing-run-results/${TESTING_RUN_HOST}
+rm -f testing-run-results/${TESTING_RUN_HOST}/testing-latest-timestamp.txt
+printf "${timestamp}" > testing-run-results/${TESTING_RUN_HOST}/testing-latest-timestamp.txt
+cp ${TESTING_RUN_HOME}/testing-log*${timestamp}* testing-run-results/${TESTING_RUN_HOST}
+cp ${TESTING_RUN_HOME}/testing-results*${timestamp}* testing-run-results/${TESTING_RUN_HOST}
 cd testing-run-results
 git add ${TESTING_RUN_HOST}
 (git commit -am "Automatic commit: ${TESTING_RUN_HOST} ${TESTING_RUN_DATE}" && git push)
 cd ../
 rm -rf testing-run-results
 
-exit ${TESTING_SCRIPT_STATUS}
